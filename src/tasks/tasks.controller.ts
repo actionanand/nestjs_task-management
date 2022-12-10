d91d370cac5b4518b3bbc9c36e362d86cd2ab862
AnandRaja-tech <anand.official.in@gmail.com>
2022-12-10 17:31:00 +0000

    basic rest api

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { CreateTaskDto } from './dto/create-task.dto';  
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly taskServ: TasksService) { }
+
+  @Get()
+  onGetAllTasks(): Task[] {
+    return this.taskServ.getAllTasks();
+  }
+
+  @Post()
+  onCreateTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.taskServ.createTask(createTaskDto);
+  }
+
+}
