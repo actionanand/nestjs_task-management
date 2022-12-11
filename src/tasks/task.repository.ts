919cdd137126f3fc0bc3ca74c6a0af58dc881a26
AnandRaja-tech <anand.official.in@gmail.com>
2022-12-11 19:07:04 +0000

    lint error fix

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,8 +2,5 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import { Task } from './task.entity';
 
-
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {
-
-}
+export class TaskRepository extends Repository<Task> {}
