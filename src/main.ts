bac147678d3b164c66d66d7dd7b644e0d76b38df
AnandRaja-tech <anand.official.in@gmail.com>
2022-12-13 13:07:17 +0000

    logging done

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 // this interceptor to keep only userId from user while saving tasks
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+
+  const nestJsPort = 3000;
+  await app.listen(nestJsPort);
+  logger.log(`Application is listening at port ${nestJsPort}`);
 }
 bootstrap();
